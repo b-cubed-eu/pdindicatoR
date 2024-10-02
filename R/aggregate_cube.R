00825d0adb3a10984736de581a05dde3e7a0a1b2
LBreugelmans <117663331+LBreugelmans@users.noreply.github.com>
2024-10-02 20:53:05 +0200

    Create aggregate_cube function
    Moved the code to aggregate occurrences over species (and produce a cube with a species list for each grid cell) to a seperate function.
    
    Signed-off-by: LBreugelmans <117663331+LBreugelmans@users.noreply.github.com>

---
+++ R/aggregate_cube.R
@@ -0,0 +1,26 @@
+
+#' Aggregate datacube over grid cell to create new dataframe with species list per grid
+#'
+#' This function aggregates a provided datacube over grid cell id, so that a new datacube 
+#' is outputted with 3 variables that contain the lists of species that are observed for 
+#' each grid cell (as speciesKeys, ott_id's and names). 
+#' 
+#' @param mcube An occurence datacube with appended ott_id's, as produced by the append_ott_id function
+#' @param cube A dataframe with for selected taxa, the number of occurrences per
+#' taxa and per grid cell
+#' @return A dataframe with for each grid cell 
+#' @example
+#' @export
+
+PD_cube <- aggr_cube %>% mutate(PD = purrr::map(unique_names, calculate_pd, tree=tree))
+
+aggregate_cube <- function(mcube){
+  simpl_cube <- mcube[,c("eeaCellCode","speciesKey","ott_id", "unique_name")] 
+  simpl_cube$eeaCellCode <- factor(simpl_cube$eeaCellCode)
+  aggr_cube <- simpl_cube %>% group_by(eeaCellCode) %>%
+  summarize(speciesKeys = list(speciesKey), ott_ids = list(ott_id), names = list(unique_name)) %>%
+  mutate(unique_spkeys = lapply(speciesKeys, unique)) %>%     
+  mutate(unique_ott_ids = lapply(ott_ids, unique)) %>%
+  mutate(unique_names = lapply(names, unique) 
+  return(aggr_cube)
+}
