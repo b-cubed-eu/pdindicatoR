15e0a9a3450b494bc0a06803d043fa4752bf3728
LBreugelmans <117663331+LBreugelmans@users.noreply.github.com>
2024-10-30 12:27:01 +0100

    Comment out redundant find tip id lines
    Redundant because getMRCA also works op tip labels, and we now have orig_tiplabel

--- R/get_PD.R
+++ R/get_PD.R
@@ -5,13 +5,16 @@ get_pd <- function(tree, species){
 
 all_matched_sp<-unique(mcube[["orig_tiplabel"]])
 
+# the following paragraph is redundant because getMRCA can also use tip labels
+# directly
+
 # get tip id's from tip labels
 
-all_tip_ids <- vector(mode="integer", length=length(all_matched_sp))
-for (i in seq_along(all_matched_sp)) {
-  x <- which(tree$tip.label == all_matched_sp[i])
-  all_tip_ids[i] <- x
-}
+# all_tip_ids <- vector(mode="integer", length=length(all_matched_sp))
+# for (i in seq_along(all_matched_sp)) {
+#  x <- which(tree$tip.label == all_matched_sp[i])
+#  all_tip_ids[i] <- x
+# }
 
 # find most recent common ancestor
 MRCA <- getMRCA(tree, all_tip_ids)
