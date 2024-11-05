5202938f5799377cf2bcb848dfc62ab1e7b1b92b
LBreugelmans <117663331+LBreugelmans@users.noreply.github.com>
2024-11-05 03:40:42 +0100

    Add documentation

--- R/get_PD.R
+++ R/get_PD.R
@@ -1,20 +1,20 @@
 
+#' Find MRCA for data cube and call function to calculate PD metrics
+#'
+#' This function determines the MRCA of all species in the datacube
+#' and calls the function(s) to calculate PD metrics
+#'
+#' @param tree A phylogenetic tree with branch lengths
+#' @param species A character vector with species names
+#' @return Calculated PD value
+#' @example get_pd(tree, species)
+#'
+
 get_pd <- function(tree, species){
 
 # get all species in matched cube
 
 all_matched_sp<-unique(mcube[["orig_tiplabel"]])
-
-# the following paragraph is redundant because getMRCA can also use tip labels
-# directly
-
-# get tip id's from tip labels
-
-# all_tip_ids <- vector(mode="integer", length=length(all_matched_sp))
-# for (i in seq_along(all_matched_sp)) {
-#  x <- which(tree$tip.label == all_matched_sp[i])
-#  all_tip_ids[i] <- x
-# }
 
 # find most recent common ancestor
 MRCA <- getMRCA(tree, all_matched_sp)
