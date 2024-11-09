b52c06f8f73d6717fa6d7b11c37de4bf66947da0
LBreugelmans <117663331+LBreugelmans@users.noreply.github.com>
2024-11-09 20:17:07 +0100

    Change name argument

--- R/convert_multipolygons.R
+++ R/convert_multipolygons.R
@@ -9,8 +9,8 @@
 convert_multipolygons <- function(object) {
   tmp1 <- tempfile(fileext = ".gpkg")
   tmp2 <- tempfile(fileext = ".gpkg")
-  sf::st_write(X, tmp1)
+  sf::st_write(object, tmp1)
   gdalUtilities::ogr2ogr(tmp1, tmp2, f = "GPKG", nlt = "MULTIPOLYGON")
   Y <- sf::st_read(tmp2)
-  sf::st_sf(sf::st_drop_geometry(X), geom = sf::st_geometry(Y))
+  sf::st_sf(sf::st_drop_geometry(object), geom = sf::st_geometry(Y))
 }
