ae1d31f0a4495fcb3bd044f006ea4f9d3436edec
Ward Langeraert <ward.langeraert@inbo.be>
2024-11-12 10:41:12 +0100

    add read.csv utils dependency

--- R/retrieve_example_data.R
+++ R/retrieve_example_data.R
@@ -1,7 +1,8 @@
 #' Retrieve example data
 #'
 #' This function specifies the paths to the example data and reads the example
-#' data files in and processes them so they are ready to be used in the workflow.
+#' data files in and processes them so they are ready to be used in the
+#' workflow.
 #'
 #' @return Objects tree (a phylogenetic tree of the order Fagales), cube (an
 #' occurrence datacube, see query specifications:
@@ -9,7 +10,8 @@
 #' (EEA 1km grid for study area) and pa (Natura2000 protected area polygons for
 #'study area
 #' @importFrom magrittr %>%
-#' @importFrom dplyr group_by reframe arrange rename mutate join_by left_join distinct
+#' @importFrom utils read.csv
+#' @import dplyr
 #' @examples
 #' ex_data <- retrieve_example_data()
 #' print(ex_data$tree)
@@ -50,6 +52,8 @@ retrieve_example_data <- function() {
   cube <- utils::read.csv(cube_path, stringsAsFactors = FALSE, sep="\t")
   grid <- sf::st_read(grid_path)
   pa <- sf::st_read(pa_path)
-  matched_nona <- read.csv(matched_nona_path, stringsAsFactors = FALSE, sep=",")
-  return(list(tree = tree, cube = cube, grid = grid, pa = pa, matched_nona=matched_nona))
+  matched_nona <- utils::read.csv(matched_nona_path, stringsAsFactors = FALSE,
+                                  sep=",")
+  return(list(tree = tree, cube = cube, grid = grid, pa = pa,
+              matched_nona=matched_nona))
 }
